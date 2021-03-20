9f595cbfd94b2f90f83e1fb504d6b1afaa4934fc
Thiago Felix <felix.gjabr@gmail.com>
2021-03-20 10:20:27 -0300

    Projeto iniciado, mostrando a mensagem Ola mundo

--- Secao08/Program.cs
+++ Secao08/Program.cs
@@ -3,7 +3,10 @@
 namespace Secao08 {
     class Program {
         static void Main(string[] args) {
+
             Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
