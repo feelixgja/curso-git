2f22df6db1f4c5f8856df30b7970a35b84b886f3
Thiago Felix <felix.gjabr@gmail.com>
2021-03-20 10:22:04 -0300

    Acrescentada mensagem de bom dia

--- Secao08/Program.cs
+++ Secao08/Program.cs
@@ -5,6 +5,7 @@ namespace Secao08 {
         static void Main(string[] args) {
 
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
 
             Console.ReadLine();
         }
